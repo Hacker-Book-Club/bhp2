aecacf9e727a3a801e48e699435aafe62c648525
kyputer <ks4829@rit.edu>
2023-01-16 19:44:25 -0500

    ch1 fin

---
+++ ch1/pep8-guide.py
@@ -0,0 +1,21 @@
+# 1) form of: from XXX import YYY
+from lxml import etree
+from subprocess import Popen
+
+# 2) 
+import argparse
+import os
+
+# 3) functions
+def get_ip(machine_name):
+    pass
+
+# 4) class definition
+class Scanner:
+    def __init__(self):
+        pass
+
+# 5) main block. provides command line execution default flow. importable as: import pep8-guide
+if __name__ == '__main__':
+    scan = Scanner()
+    print('hello')
