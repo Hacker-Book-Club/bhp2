aba1fd55190de7057ab921c4be4cad5aaa852a0e
Kyputer <ks4829@rit.edu>
2023-04-17 19:58:33 -0400

    on to kicking the tires. end of pg 24

--- ch2/proxy.py
+++ ch2/proxy.py
@@ -92,7 +92,6 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             print("[*] No more data. Closing connection.")
             break
 
-## TODO: Pg23 -> server_loop function
 def server_loop(local_host, local_port,
                 remote_host, remote_port, receive_first):
     # 1) Creating a socket for the server_loop function
@@ -119,5 +118,29 @@ def server_loop(local_host, local_port,
         proxy_thread = threading.Thread(
             target=proxy_handler,
             args=(client_socket, remote_host,
-            remote_port, receive_first)
-        )
+            remote_port, receive_first))
+        proxy_thread.start()
+    
+def main():
+    if len(sys.argv[1:]) != 5:
+        print("Usage: ./proxy.py [localhost] [localport]", end='')
+        print("[remotehost] [remoteport] [receive_first]")
+        print("Example: ./proxy.py 127.0.0.1 9000 10.12.132.1 9000 True")
+        sys.exit(0)
+    local_host = sys.argv[1]
+    local_port = int(sys.argv[2])
+    remote_host = sys.argv[3]
+    remote_port = int(sys.argv[4])
+    
+    receive_first = sys.argv[5]
+    
+    if "True" in receive_first:
+        receive_first = True
+    else:
+        receive_first = False
+    
+    server_loop(local_host, local_port,
+        remote_host, remote_port, receive_first)
+    
+if __name__ == '__main__':
+    main()
