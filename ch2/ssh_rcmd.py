33298e40efa254f4c415b418f89416dfe35e0b7a
Kyputer <ks4829@rit.edu>
2023-04-24 19:57:25 -0400

    ssh rcmd done

--- ch2/ssh_rcmd.py
+++ ch2/ssh_rcmd.py
@@ -12,13 +12,16 @@ def ssh_command(ip, port, user, passwd, command):
         ssh_session.send(command)
         print(ssh_session.recv(1024))
         while True:
+            # 1) This infinite loop will take commands from the connection and execute them.
             command = ssh_session.recv(1024)
             try:
                 cmd = command.decode()
                 if cmd == 'exit':
                     client.close()
                     break
+                # 2) send output back to the caller
                 cmd_output = subprocess.check_output(shlex.split(cmd), shell=True)
+                # 3) This is the confirmation that it was received
                 ssh_session.send(cmd_output or 'okay')
             except Exception as e:
                 ssh_session.send(str(e))
@@ -32,4 +35,5 @@ if __name__ == '__main__':
 
     ip = input('Enter server IP: ')
     port = input('Enter port: ')
+    # "client connected on first command run"
     ssh_command(ip, port, user, password, 'ClientConnected')
